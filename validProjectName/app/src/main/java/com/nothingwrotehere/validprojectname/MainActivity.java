9277ec575762e7577467d78f84005541df0a0468
Hello Wordovic <tf.2.98@mail.ru>
2016-10-02 18:12:40 +0300

    Created new Activity. Added new xml & .class files.

--- validProjectName/app/src/main/java/com/nothingwrotehere/validprojectname/MainActivity.java
+++ validProjectName/app/src/main/java/com/nothingwrotehere/validprojectname/MainActivity.java
@@ -1,15 +1,24 @@
 package com.nothingwrotehere.validprojectname;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+Button btnAct2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAct2 = (Button)findViewById(R.id.btnAct2);
+        btnAct2.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, ActivityToNext.class);
+                startActivity(intent);
 
-    onCrea
-}
+        }
+    }
